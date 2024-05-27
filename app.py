67ec15de4798b507f04ef7556bb995ffcfe5d13c
dsudzeng <38187550+dsudzeng@users.noreply.github.com>
2024-05-27 00:33:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,14 @@ txt_input = st.text_area('Enter your question', '', height=200)
 
 interpreter_assistant = OpenAIAssistantRunnable.create_assistant(
     name="langchain assistant",
-    instructions="You are a personal math tutor. Write and run code to answer math questions.",
+    instructions='''You are a personal math tutor. Write and run code to answer math questions. 
+    // response rules
+Writing math formulas:
+You have a MathJax render environment.
+- Any LaTeX text between single dollar sign ($) will be rendered as a TeX formula;
+- Use $(tex_formula)$ in-line delimiters to display equations instead of backslash;
+- The render environment only uses $ (single dollarsign) as a container delimiter, never output $$.
+Example: $x^2 + 3x$ is output for "x² + 3x" to appear as TeX.`''',
     tools=[{"type": "code_interpreter"}],
     model="gpt-4-1106-preview",
 )
@@ -22,5 +29,6 @@ output = interpreter_assistant.invoke(input_dict)
 
 i = 0
 while i < len(output):
-  st.markdown(output[i].content[0].text.value)
+  st.info(output[i].content[0].text.value)
   i = i + 1
+
