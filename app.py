47fc0ec17a9cecc24b2e2d80b928d2825d81ce61
dsudzeng <38187550+dsudzeng@users.noreply.github.com>
2025-04-06 15:49:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,14 @@ from langchain_core.output_parsers import StrOutputParser
 os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
 
 st.set_page_config(page_title='🦜🔗 GenAI Math Tutor')
+st.header('ARTIFICIAL INTELLIGENCE IN ORGANIZATIONS (BS)', divider='rainbow')
+new_title = '<p style="font-family:sans-serif; color:Green; font-size: 30px;">The future of business</p>'
+st.markdown(new_title, unsafe_allow_html=True)
+
+
+st.write('**Students will learn to understand what artificial intelligence is, and what AI means for businesses and organizations and develop skills for applying artificial intelligence tools and methods in a variety of industries. With these skills, they will be prepared to help make data-driven decisions in their companies, optimize business processes and workflows, minimize costs, and maximize revenues by utilizing AI tools and applications. They will also be able to successfully navigate AI-related issues, challenges, and ethics in workplaces and be AI-aware in society.**')
+
+#st.title('🦜🔗 Paradigm Futures Image Generator')
 st.title('🦜🔗 GenAI Math Tutor')
 txt_input = st.text_area('Enter your question', '', height=200)
 
